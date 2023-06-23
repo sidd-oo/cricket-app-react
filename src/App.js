c139e71ee1ada64c4ad1423913547a2a54b1460e
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2023-06-23 16:54:07 +0530

    feat: implemented client-side routing using React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Home from './pages/Home';
+import TableContainer from './components/Table/TableContainer'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    children: [
+      {
+        path: "",
+        element: <TableContainer />,
+      },
+    ],
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <RouterProvider router={router}>
+      <div className="App">
+      </div>
+    </RouterProvider>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
-import TableContainer from "../components/Table/TableContainer";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <TableContainer />
+      <Outlet />
     </>
   );
 };
