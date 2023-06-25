1fb98f3de30c9c29c748bc486840a4f2595f5d8b
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2023-06-25 21:09:54 +0530

    feat: added naviagtion route for header's home button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { BiSolidHome } from 'react-icons/bi';
-import {CgProfile} from 'react-icons/cg'
+import { BiSolidHome } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="bg-gray-100 flex  justify-between border-2 border-b-gray-200 px-5 py-3">
+    <div className="flex justify-between  border-2 border-b-gray-200 bg-gray-100 px-5 py-3">
       <h1 className="text-2xl font-bold text-black ">Nirmata InfoCricket</h1>
-      <div className="flex justify-evenly text-md font-semibold text-gray-700">
-        <h1 className="ml-4 flex justify-center items-center text-2xl"><BiSolidHome/></h1>
-        <h1 className="ml-4 flex justify-center items-center">Live Score</h1>
-        <h1 className="ml-4 flex justify-center items-center">News</h1>
-        <h1 className="ml-4 flex justify-center items-center">Upcomming</h1>
-        <h1 className="ml-4 flex justify-center items-center text-2xl"><CgProfile/></h1>
+      <div className="text-md flex justify-evenly font-semibold text-gray-700">
+        <h1 className="ml-4 flex items-center justify-center text-2xl">
+          <Link to="/">
+            <BiSolidHome />
+          </Link>
+        </h1>
+        <h1 className="ml-4 flex items-center justify-center">Live Score</h1>
+        <h1 className="ml-4 flex items-center justify-center">News</h1>
+        <h1 className="ml-4 flex items-center justify-center">Upcomming</h1>
+        <h1 className="ml-4 flex items-center justify-center text-2xl">
+          <CgProfile />
+        </h1>
       </div>
     </div>
   );
